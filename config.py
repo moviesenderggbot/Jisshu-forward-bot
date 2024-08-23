7e63f97c5a57da9a747e4143837671b429f7b777
xyzdevoloper <157599470+moviesenderggbot@users.noreply.github.com>
2024-08-23 18:19:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://0qxwx42ukf:oxfFP9wSRVYt7awV@cluster0.abjqxfe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002050604893'))
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '2050604893'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/+bnmbjw-mkckyNTc1") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
 

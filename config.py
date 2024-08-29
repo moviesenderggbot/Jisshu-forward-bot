f34515822da06ea00e6fcc2b14ea747bd31d261c
xyzdevoloper <157599470+moviesenderggbot@users.noreply.github.com>
2024-08-29 09:27:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     
     API_ID = environ.get("API_ID", "22620068")
     API_HASH = environ.get("API_HASH", "11e2c113078324f7e36688baa86c3911")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7276231385:AAG7PYYPmwl_q73J-Asz-FIHK8U8zlxQ-Z4") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7274390732:AAHq3xQfTtkckKAe-7vdkeYcyb4GkpY0QPY") 
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6062527012').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
 
@@ -15,7 +15,7 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002050604893'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/+bnmbjw-mkckyNTc1") # FORCE SUB channel link 
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/sfgdhdgjddsga") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
 
 
